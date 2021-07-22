522929dbb92b5c0031d4997c32e72620e57aedf2
Noam Lubofsky <noamlubofsky@gmail.com>
2021-07-22 13:34:58 -0400

    commit tree 02c480aa02c99ae2383bcdb03b5dfe9735662cfe

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+    
+    attr_accessor :total, :discount, :items
+    
+    def initialize(discount = 20)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
 
+    def add_item(item, price)
+        @items << item
+        @total += price
+    end
+
+end
